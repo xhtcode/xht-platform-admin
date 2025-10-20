f7df0f92843763d812e635d5aceada19900e7600
小糊涂 <616326125@qq.com>
2025-10-20 15:29:32 +0800

    feat(layout): 添加布局类型切换功能- 新增默认布局和分栏布局两种布局样式 - 实现布局类型切换时的加载状态提示 - 添加布局选择器的样式和交互效果- 更新主题配置逻辑以支持新的布局类型- 引入 Element Plus 的 CircleCheckFilled 图标组件- 在重置设置功能中添加布局类型恢复默认值逻辑

--- src/layout/components/app-setting/index.vue
+++ src/layout/components/app-setting/index.vue
@@ -16,6 +16,34 @@
         size="default"
       >
         <el-divider>布局样式</el-divider>
+        <div class="xht-layout-box">
+          <div
+            class="xht-layout-item xht-layout-default"
+            :class="{ 'is-active': layoutType === 'default' }"
+            @click="changeLayoutType('default')"
+          >
+            <div class="layout-dark"></div>
+            <div class="layout-container">
+              <div class="layout-light"></div>
+              <div class="layout-content"></div>
+            </div>
+            <el-icon>
+              <CircleCheckFilled />
+            </el-icon>
+          </div>
+          <div
+            class="xht-layout-item xht-layout-columns"
+            :class="{ 'is-active': layoutType === 'columns' }"
+            @click="changeLayoutType('columns')"
+          >
+            <div class="layout-dark"></div>
+            <div class="layout-light"></div>
+            <div class="layout-content"></div>
+            <el-icon>
+              <CircleCheckFilled />
+            </el-icon>
+          </div>
+        </div>
         <el-form-item label="水印内容">
           <el-input v-model="watermarkContent" disabled placeholder="请输入水印内容" />
         </el-form-item>
@@ -98,6 +126,7 @@ import { AppLanguageEnums, ComponentSizeEnums } from '@/service/enums'
 import { storeToRefs } from 'pinia'
 import useThemeColorHooks from '@/hooks/use-theme-hooks'
 import { DefaultThemeColor } from '@/service/constant'
+import { CircleCheckFilled } from '@element-plus/icons-vue'
 
 defineOptions({
   name: 'AppSetting',
@@ -114,6 +143,7 @@ const state = reactive<StateType>({
 })
 const themeStore = useThemeStore()
 const {
+  layoutType,
   darkStatus,
   breadcrumb,
   tagsViewStatus,
@@ -140,10 +170,26 @@ const predefineColors = ref([
  * 禁用 颜色选择器
  */
 const displayColorPicker = computed(() => mournModeStatus.value || colorWeaknessModeStatus.value)
+
+/**
+ * 切换布局类型
+ */
+const changeLayoutType = (args: 'default' | 'columns') => {
+  if (args === layoutType.value) return
+  const loadingInstance = ElLoading.service({
+    text: '正在处理中...',
+    fullscreen: true,
+  })
+  window.setTimeout(() => {
+    layoutType.value = args
+    loadingInstance.close()
+  }, 500)
+}
 /**
  * 恢复默认配置
  */
 const resetSetting = () => {
+  layoutType.value = 'default'
   darkStatus.value = false
   breadcrumb.value = true
   tagsViewStatus.value = true
@@ -155,3 +201,99 @@ const resetSetting = () => {
   colorWeaknessModeStatus.value = false
 }
 </script>
+<style lang="scss" scoped>
+.xht-layout-box {
+  position: relative;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 20px;
+
+  .xht-layout-item {
+    position: relative;
+    box-sizing: border-box;
+    width: 100px;
+    height: 70px;
+    padding: 6px;
+    cursor: pointer;
+    border-radius: 5px;
+    box-shadow: 0 0 5px 1px var(--el-border-color-dark);
+    transition: all 0.2s;
+
+    .layout-dark {
+      background-color: var(--el-color-primary);
+      border-radius: 3px;
+    }
+
+    .layout-light {
+      background-color: var(--el-color-primary-light-5);
+      border-radius: 3px;
+    }
+
+    .layout-content {
+      background-color: var(--el-color-primary-light-8);
+      border: 1px dashed var(--el-color-primary);
+      border-radius: 3px;
+    }
+
+    .el-icon {
+      position: absolute;
+      right: 10px;
+      bottom: 10px;
+      color: var(--el-color-primary);
+      transition: all 0.2s;
+    }
+
+    &:hover {
+      box-shadow: 0 0 5px 1px var(--el-text-color-secondary);
+    }
+  }
+
+  .is-active {
+    box-shadow: 0 0 0 2px var(--el-color-primary) !important;
+  }
+
+  .xht-layout-default {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 20px;
+
+    .layout-dark {
+      width: 20%;
+    }
+
+    .layout-container {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      width: 72%;
+
+      .layout-light {
+        height: 20%;
+      }
+
+      .layout-content {
+        height: 67%;
+      }
+    }
+  }
+
+  .xht-layout-columns {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 15px;
+
+    .layout-dark {
+      width: 14%;
+    }
+
+    .layout-light {
+      width: 17%;
+    }
+
+    .layout-content {
+      width: 55%;
+    }
+  }
+}
+</style>
