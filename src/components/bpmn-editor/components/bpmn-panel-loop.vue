38ac9e5121243fcebce511ead3bba29344c079f1
小糊涂 <616326125@qq.com>
2026-08-19 15:03:45 +0800

    feat(bpmn): 添加BPMN多实例循环配置功能
    - 创建useBpmnLoopHooks钩子函数处理多实例相关逻辑
    - 实现获取和更新多实例类型（无、并行、串行）功能
    - 添加获取和更新多实例属性（基数、集合、元素变量、完成条件）功能
    - 集成多实例配置到BPMN面板界面
    - 实现多实例属性的实时更新和同步
    - 添加多实例类型的切换和初始化逻辑
    - 优化表单输入的变更响应机制

--- src/components/bpmn-editor/components/bpmn-panel-loop.vue
+++ src/components/bpmn-editor/components/bpmn-panel-loop.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
-import { BellFilled, HelpFilled, Promotion } from '@element-plus/icons-vue'
+import { HelpFilled } from '@element-plus/icons-vue'
 import BpmnPanelTitle from '@/components/bpmn-editor/components/bpmn-panel-title.vue'
 import { useBpmnStore } from '@/store/modules/bpmn.store'
 import { storeToRefs } from 'pinia'
+import { useBpmnLoopHooks } from '@/components/bpmn-editor/components/bpmn.loop.hooks'
 
 defineOptions({
   name: 'BpmnPanelLoop',
   inheritAttrs: false,
 })
 const bpmnStore = useBpmnStore()
-const { activeElement, activeElementId } = storeToRefs(bpmnStore)
+const { activeElementId } = storeToRefs(bpmnStore)
+const { getLoopType, getLoopProperties, updateLoopType, updateLoopProperties } = useBpmnLoopHooks()
 /**
  * 多实例类型
  */
@@ -32,16 +34,54 @@ const loopVariable = ref<string>('')
 const loopCompletionCondition = ref<string>('')
 
 /**
+ * 初始化多实例属性
+ */
+const initLoopProperties = () => {
+  loopType.value = getLoopType()
+  const loopProperties = getLoopProperties()
+  loopCardinality.value = loopProperties.loopCardinality
+  loopCollection.value = loopProperties.loopCollection
+  loopVariable.value = loopProperties.loopVariable
+  loopCompletionCondition.value = loopProperties.loopCompletionCondition
+}
+
+/**
  * 多实例类型改变
  */
 const loopTypeChange = () => {
+  updateLoopType(loopType.value)
   loopCardinality.value = ''
   loopCollection.value = ''
   loopVariable.value = ''
   loopCompletionCondition.value = ''
+  updateLoopProperties({
+    loopCardinality: loopCardinality.value,
+    loopCollection: loopCollection.value,
+    loopVariable: loopVariable.value,
+    loopCompletionCondition: loopCompletionCondition.value,
+  })
 }
-</script>
 
+/**
+ * 更新多实例属性
+ */
+const loopPropertiesChange = () => {
+  updateLoopProperties({
+    loopCardinality: loopCardinality.value,
+    loopCollection: loopCollection.value,
+    loopVariable: loopVariable.value,
+    loopCompletionCondition: loopCompletionCondition.value,
+  })
+}
+
+watch(
+  () => activeElementId.value,
+  () => {
+    initLoopProperties()
+  },
+  { immediate: true }
+)
+</script>
 <template>
   <el-collapse-item name="bpmn-panel-loop">
     <template #title>
@@ -52,8 +92,8 @@ const loopTypeChange = () => {
         <el-tag v-show="loopType !== 'none'" size="small" type="danger" round effect="dark" class="mr-1">&nbsp;</el-tag>
       </div>
     </template>
-    <el-form-item label="多实例类型" class="mb-2!" @change="loopTypeChange">
-      <el-radio-group v-model="loopType">
+    <el-form-item label="多实例类型" class="mb-2!">
+      <el-radio-group v-model="loopType" @change="loopTypeChange">
         <el-radio-button label="无" value="none" />
         <el-radio-button label="并行" value="parallel" />
         <el-radio-button label="串行" value="sequential" />
@@ -61,16 +101,16 @@ const loopTypeChange = () => {
     </el-form-item>
     <template v-if="loopType !== 'none'">
       <el-form-item label="基数" class="mb-2!">
-        <el-input v-model="loopCardinality" :maxlength="200" show-word-limit placeholder="请输入基数" />
+        <el-input v-model="loopCardinality" :maxlength="200" show-word-limit placeholder="请输入基数" @change="loopPropertiesChange" />
       </el-form-item>
       <el-form-item label="集合" class="mb-2!">
-        <el-input v-model="loopCollection" :maxlength="200" show-word-limit placeholder="请输入集合" />
+        <el-input v-model="loopCollection" :maxlength="200" show-word-limit placeholder="请输入集合" @change="loopPropertiesChange" />
       </el-form-item>
       <el-form-item label="元素变量" class="mb-2!">
-        <el-input v-model="loopVariable" :maxlength="200" show-word-limit placeholder="请输入元素变量" />
+        <el-input v-model="loopVariable" :maxlength="200" show-word-limit placeholder="请输入元素变量" @change="loopPropertiesChange" />
       </el-form-item>
       <el-form-item label="完成条件" class="mb-2!">
-        <el-input v-model="loopCompletionCondition" :maxlength="200" show-word-limit placeholder="请输入完成条件" />
+        <el-input v-model="loopCompletionCondition" :maxlength="200" show-word-limit placeholder="请输入完成条件" @change="loopPropertiesChange" />
       </el-form-item>
     </template>
   </el-collapse-item>
--- src/components/bpmn-editor/components/bpmn.loop.hooks.ts
+++ src/components/bpmn-editor/components/bpmn.loop.hooks.ts
@@ -1,1 +1,143 @@
-export const useBpmnLoopHooks = () => {}
+import { ModdleElement } from 'bpmn-js/lib/util/ModelUtil'
+import { useBpmnPlusHooks } from '@/hooks/use.bpmn'
+import { useBpmnStore } from '@/store/modules/bpmn.store'
+import { storeToRefs } from 'pinia'
+
+/**
+ * 多实例类型
+ */
+type BpmnLoopType = 'none' | 'parallel' | 'sequential'
+
+/**
+ * 多实例属性
+ */
+interface BpmnLoopProperties {
+  loopCardinality: string
+  loopCollection: string
+  loopVariable: string
+  loopCompletionCondition: string
+}
+
+/**
+ * bpmn 多实例(循环)相关
+ */
+export const useBpmnLoopHooks = () => {
+  const bpmnStore = useBpmnStore()
+  const { getElementBusinessObject, createModdleElement } = useBpmnPlusHooks()
+  const { modeling, activeElement } = storeToRefs(bpmnStore)
+
+  /**
+   * 获取多实例特性
+   */
+  function getLoopCharacteristics(): ModdleElement | undefined {
+    const businessObject = getElementBusinessObject(activeElement.value)
+    return businessObject?.get('loopCharacteristics')
+  }
+
+  /**
+   * 获取多实例类型
+   */
+  function getLoopType(): BpmnLoopType {
+    const loopCharacteristics = getLoopCharacteristics()
+    if (!loopCharacteristics) return 'none'
+    if (loopCharacteristics.isSequential) return 'sequential'
+    if (
+      loopCharacteristics.loopCardinality ||
+      loopCharacteristics.collection ||
+      loopCharacteristics.elementVariable ||
+      loopCharacteristics.completionCondition
+    ) {
+      return 'parallel'
+    }
+    return 'none'
+  }
+
+  /**
+   * 获取多实例属性
+   */
+  function getLoopProperties(): BpmnLoopProperties {
+    const loopCharacteristics = getLoopCharacteristics()
+    let loopCardinality = ''
+    let loopCollection = ''
+    let loopVariable = ''
+    let loopCompletionCondition = ''
+    if (loopCharacteristics) {
+      if (loopCharacteristics.loopCardinality) {
+        loopCardinality = loopCharacteristics.loopCardinality.body
+      }
+      if (loopCharacteristics.collection) {
+        loopCollection = loopCharacteristics.collection
+      }
+      if (loopCharacteristics.elementVariable) {
+        loopVariable = loopCharacteristics.elementVariable
+      }
+      if (loopCharacteristics.completionCondition) {
+        loopCompletionCondition = loopCharacteristics.completionCondition.body
+      }
+    }
+    return { loopCardinality, loopCollection, loopVariable, loopCompletionCondition }
+  }
+
+  /**
+   * 更新多实例类型
+   * @param loopType 多实例类型
+   */
+  function updateLoopType(loopType: BpmnLoopType) {
+    const businessObject = getElementBusinessObject(activeElement.value)
+    const loopCharacteristics = getLoopCharacteristics()
+    // 移除多实例配置
+    if (loopType === 'none') {
+      if (loopCharacteristics) {
+        modeling.value?.updateModdleProperties(activeElement.value!, businessObject, {
+          loopCharacteristics: undefined,
+        })
+      }
+      return
+    }
+    // 创建多实例特性
+    let currentLoopCharacteristics = loopCharacteristics
+    if (!currentLoopCharacteristics) {
+      currentLoopCharacteristics = createModdleElement('bpmn:MultiInstanceLoopCharacteristics', {}, businessObject)
+      modeling.value?.updateModdleProperties(activeElement.value!, businessObject, {
+        loopCharacteristics: currentLoopCharacteristics,
+      })
+    }
+    // 更新串行/并行
+    modeling.value?.updateModdleProperties(activeElement.value!, currentLoopCharacteristics, {
+      isSequential: loopType === 'sequential',
+    })
+  }
+
+  /**
+   * 更新多实例属性
+   * @param loopProperties 多实例属性
+   */
+  function updateLoopProperties(loopProperties: BpmnLoopProperties) {
+    const { loopCardinality, loopCollection, loopVariable, loopCompletionCondition } = loopProperties
+    const loopCharacteristics = getLoopCharacteristics()
+    if (!loopCharacteristics) return
+    // 创建基数表达式
+    let newLoopCardinality
+    if (loopCardinality) {
+      newLoopCardinality = createModdleElement('bpmn:FormalExpression', { body: loopCardinality }, loopCharacteristics)
+    }
+    // 创建完成条件表达式
+    let newCompletionCondition
+    if (loopCompletionCondition) {
+      newCompletionCondition = createModdleElement('bpmn:FormalExpression', { body: loopCompletionCondition }, loopCharacteristics)
+    }
+    modeling.value?.updateModdleProperties(activeElement.value!, loopCharacteristics, {
+      loopCardinality: newLoopCardinality,
+      collection: loopCollection,
+      elementVariable: loopVariable,
+      completionCondition: newCompletionCondition,
+    })
+  }
+
+  return {
+    getLoopType,
+    getLoopProperties,
+    updateLoopType,
+    updateLoopProperties,
+  }
+}
